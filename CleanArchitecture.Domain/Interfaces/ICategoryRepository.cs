403ef08c4eab521ef407bd06b1a79b5a2e44d617
Kayky Barbosa <kaykyb629@gmail.com>
2023-09-19 20:23:23 -0300

    Add: Entity Repositories

---
+++ CleanArchitecture.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetCategoryById(int? id);
+
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
---
+++ CleanArchitecture.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAync();
+        Task<Product> GetProductByIdAync(int? id);
+        Task<Product> GetProductCategoryAync(int? id);
+
+        Task<Product> CreateAync(Product Product);
+        Task<Product> UpdateAync(Product Product);
+        Task<Product> RemoveAync(Product Product);
+    }
+}
