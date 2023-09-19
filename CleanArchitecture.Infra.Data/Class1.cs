34d43c2d20d1bc67d4a49ac1e8078ff9f05e9edd
Kayky Barbosa <kaykyb629@gmail.com>
2023-09-19 18:16:49 -0300

    Initial Commit

---
+++ CleanArchitecture.Infra.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchitecture.Infra.Data
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CleanArchitecture.Infra.IoC/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchitecture.Infra.IoC
+{
+    public class Class1
+    {
+
+    }
+}
