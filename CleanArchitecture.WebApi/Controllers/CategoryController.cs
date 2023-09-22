451809be9dd7c2312b508f0b4325a931f89a764b
Kayky Barbosa <kaykyb629@gmail.com>
2023-09-22 08:33:31 -0300

    Add: CategoryController and View

---
+++ CleanArchitecture.WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebApi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _service;
+        public CategoryController(ICategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _service.GetCategories();
+            return View(categories);
+        }
+    }
+}
