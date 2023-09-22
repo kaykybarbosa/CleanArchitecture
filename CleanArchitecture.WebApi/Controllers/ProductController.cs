4a1122c5e19ff4979790f5ef20dbf1425ca7e196
Kayky Barbosa <kaykyb629@gmail.com>
2023-09-22 18:16:27 -0300

    Add: implementing ProductController and View

---
+++ CleanArchitecture.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebApi.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _service;
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _service.GetAll();
+            return View(products);
+        }
+    }
+}
