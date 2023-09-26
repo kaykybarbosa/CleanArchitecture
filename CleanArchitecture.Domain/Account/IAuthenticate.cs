3441bc672c825dfc36d701a4448dbf8285bad385
Kayky Barbosa <kaykyb629@gmail.com>
2023-09-26 15:24:54 -0300

    Add: implementing the Interfaces

---
+++ CleanArchitecture.Domain/Account/IAuthenticate.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Domain.Account
+{
+    public interface IAuthenticate
+    {
+        Task<bool> AuthenticateAsync(string email, string password);
+        Task<bool> RegisterUserAsync(string email, string password);
+        Task Logout();
+    }
+}
